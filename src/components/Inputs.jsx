dfb7cf2f05ea26cd5c0d10d85294596bd5897e58
Ayush Mishra <100191388+DraGonoff69@users.noreply.github.com>
2023-09-20 02:53:13 +0530

    third commit

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -14,8 +14,8 @@ function Inputs({onSearch}) {
             onSearch(query);
         }}
     return (
-        <div className='flex flex-row justify-center my-6' >
-            <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
+        <div className='flex flex-row justify-center my-6 px-4' >
+            <div className='flex flex-row w-3/4 items-center justify-center space-x-4 '>
 
                 <input type='text' value={query} onChange={(e)=>setQuery(e.target.value.toLowerCase())} placeholder="search.. " onKeyDown={handleKeyPress} className='text-xl font-light p-2 shadow-xl focus:outline-none capitalize placeholder:lowercase' />
                 <UilSearch size={25} onClick={handleSearch} className="text-white cursor-pointer transition ease-out hover:scale-125" />
